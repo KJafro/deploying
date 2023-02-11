98daf5bcaab6c45a15ace32580fc599a93eb3dba
KJafro <Kurtj222@outlook.com>
2023-02-11 11:50:42 +0000

    y

--- api/index.js
+++ api/index.js
@@ -8,6 +8,13 @@ const postRoute = require ("./routes/posts")
 const categoryRoute = require ("./routes/categories")
 const multer = require("multer")
 const path = require("path")
+const cors = require ("cors")
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+)
 
 dotenv.config();
 app.use(express.json());
