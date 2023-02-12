1d5389cca1273bb58c506234f77dd70a93b3fc1b
KJafro <Kurtj222@outlook.com>
2023-02-12 19:22:43 +0000

    ok

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ const storage = multer.diskStorage({
         cb(null, "images")
     }, 
     filename:(req, file, cb) => {
-        cb(null, filename)
+        cb(null, req.body.name)
     },
 });
 
