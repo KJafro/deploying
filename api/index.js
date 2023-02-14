3b1036f2b8754af16bd324b6febd93972fcad3a6
KJafro <Kurtj222@outlook.com>
2023-02-14 13:33:26 +0000

    ok

--- api/index.js
+++ api/index.js
@@ -10,15 +10,21 @@ const multer = require("multer")
 const path = require("path")
 const cors = require ("cors")
 const PORT = process.env.PORT || 5000;
+const cloudinary = require('./cloudinary/cloudinary').v2;
+
 app.use(
     cors({
         origin: "*"
     })
 );
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}))
 
 dotenv.config();
-app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI, {
