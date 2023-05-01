751fa904903f046c2b06cf6e5f029a798a1655c3
KJafro <Kurtj222@outlook.com>
2023-05-01 19:27:34 +0100

    ok

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ webpush.setVapidDetails(
 app.post("/subscribe", (req, res) => {
     const subscription = req.body;
     res.status(201).json({});
-    const payload = JSON.stringify({ title: "Backend!!!" });
+    const payload = JSON.stringify({ title: "New Post/Podcast added!" });
     webpush
       .sendNotification(subscription, payload)
       .catch(err => console.error(err));
