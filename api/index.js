b78dd9bcb3934ab72b06625d5b8d0447e1109290
KJafro <Kurtj222@outlook.com>
2023-08-01 11:52:10 +0100

    ok

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")))
 //   })
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://admin:admin@everydaybeing.jtw7i4x.mongodb.net/everydaybeing?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology:true,
 }).then(console.log("Connected to DB!!!!!"))
